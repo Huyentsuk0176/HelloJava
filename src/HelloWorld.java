008c119ac9522b7942a6d12d05ce3dbe497331e3
Huyentsuk0176 <Phthihuyenta03@gmail.com>
2025-12-15 00:20:55 +0900

    session1-ex4

---
+++ src/HelloWorld.java
@@ -0,0 +1,21 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        int tong = a + b;
+        int hieu = a - b;
+        int tich = a * b;
+        int thuong = a / b;
+        int du = a % b;
+
+        System.out.println("Gia tri a = " + a);
+        System.out.println("Gia tri b = " + b);
+        System.out.println("Tong a + b = " + tong);
+        System.out.println("Hieu a - b = " + hieu);
+        System.out.println("Tich a * b = " + tich);
+        System.out.println("Thuong a / b = " + thuong);
+        System.out.println("Du a % b = " + du);
+    }
+}
+
