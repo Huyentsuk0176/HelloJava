501704d79c7bcfd868f155c1add8cae54508bf1e
Huyentsuk0176 <Phthihuyenta03@gmail.com>
2025-12-15 00:11:04 +0900

    session1-ex3

--- src/HeloWorld.java
+++ src/HeloWorld.java
@@ -1,10 +1,8 @@
 public class HeloWorld {public static void main(String[] args){
-    String ten = "Pham Thi Huyen";
-    int tuoi = 34;
-    String diachi = "Tokyo, Nhat Ban";
-    System.out.printf(
-            "Tên tôi là %s, tôi %d tuổi, hiện tại đang học tại PTIT-HCM. Quê ở %s",
-            ten, tuoi, diachi
-    );
+    double r = 3;
+    double pi = 3.14;
+    double area = pi * r * r;
+    System.out.println(
+            "Dien tich: " + area);
 }
 }
