17a894174e3d784b1abbdceb6fa75582e4066a5c
Huyentsuk0176 <Phthihuyenta03@gmail.com>
2025-12-19 21:59:48 +0900

    session4Ex2.java

--- src/HeloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HeloWorld {public static void main(String[] args){
-    double r = 3;
-    double pi = 3.14;
-    double area = pi * r * r;
-    System.out.println(
-            "Dien tich: " + area);
-}
-}
