173669941346484d2587d098673c4da97c722b26
Huyentsuk0176 <Phthihuyenta03@gmail.com>
2026-01-09 17:12:26 +0900

    Session 8ex3

---
+++ src/session8_ex3/Main.java
@@ -0,0 +1,18 @@
+package session8_ex3;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+        Square square = new Square(3.0);
+
+        circle.setColor("Red");
+        rectangle.setColor("Blue");
+        square.setColor("Green");
+
+
+        System.out.println("Circle color: " + circle.getColor());
+        System.out.println("Rectangle color: " + rectangle.getColor());
+        System.out.println("Square color: " + square.getColor());
+    }
+}
